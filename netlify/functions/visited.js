2ef8ab17792b7e7cbec5d0fbfd5927d8c57d878d
Egor Kunovsky <egorka@gmail.com>
2024-08-09 11:55:21 +0000

    visit

--- netlify/functions/visited.js
+++ netlify/functions/visited.js
@@ -4,14 +4,18 @@ const h3 = require('h3-js');
 exports.handler = async (event, context) => {
   let client;
   try {
+    console.log('Connecting to database...');
     const connectionString = process.env.POSTGRES_URL_NON_POOLING;
+
     client = new Client({
       connectionString: connectionString,
     });
 
     await client.connect();
+    console.log('Connected to database.');
 
     const result = await client.query('SELECT h3_index, visits FROM h3_level_9 WHERE visits > 0');
+    console.log('Query successful:', result.rows);
 
     const visitedH3s = result.rows.map(row => {
       const h3Index = row.h3_index;
@@ -25,6 +29,7 @@ exports.handler = async (event, context) => {
     });
 
     await client.end();
+    console.log('Database connection closed.');
 
     return {
       statusCode: 200,
@@ -39,13 +44,12 @@ exports.handler = async (event, context) => {
 
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to retrieve visited H3 indexes' }),
+      body: JSON.stringify({ error: 'Failed to retrieve visited H3 indexes', details: error.message }),
     };
   }
 };
 
 function h3ToGeoBoundary(h3Index) {
-  // Using the h3-js library to get the geo boundary for the given h3Index
   const hexBoundary = h3.h3ToGeoBoundary(h3Index);
   const formattedBoundary = hexBoundary.map(coord => [coord[0], coord[1]]);
   return formattedBoundary;
